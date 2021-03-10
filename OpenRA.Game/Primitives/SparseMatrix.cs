96aae3ee26fb6a87b3b94dd19475cf0fb6f790da
ia6382 <ia6382@student.uni-lj.si>
2021-03-10 15:51:21 +0100

    reservation table

---
+++ OpenRA.Game/Primitives/SparseMatrix.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRA.Primitives
+{
+	public class SparseMatrix<T>
+	{
+		private Dictionary<Tuple<int, int, int>, T> data = new Dictionary<Tuple<int, int, int>, T>();
+
+		public T this[int a, int b, int c]
+		{
+			get
+			{
+				var key = new Tuple<int, int, int>(a, b, c);
+				return data[key];
+			}
+
+			set
+			{
+				var key = new Tuple<int, int, int>(a, b, c);
+				data[key] = value;
+			}
+		}
+
+		public void RemoveKey(int a, int b, int c)
+		{
+			var key = new Tuple<int, int, int>(a, b, c);
+			if (data.ContainsKey(key))
+				data.Remove(key);
+		}
+
+		public bool ContainsKey(int a, int b, int c)
+		{
+			var key = new Tuple<int, int, int>(a, b, c);
+			return data.ContainsKey(key);
+		}
+	}
+}
---
+++ OpenRA.Mods.Common/Traits/Player/SpaceTimeReservation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRA.Graphics;
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.Traits
+{
+	[Desc("Attach this to the player actor for path reservation during WHCA* cooperative search.")]
+	public class SpaceTimeReservationInfo : TraitInfo
+	{
+		[Desc("Modulus operator - how many ticks of time we remember before we overwrite reservations.")]
+		public readonly int TimeLength = 4999;
+
+		public override object Create(ActorInitializer init) { return new SpaceTimeReservation(init.Self, this); } // SLO: se this ce potrebuje fields iz Info classa.
+	}
+
+	public class SpaceTimeReservation
+	{
+		private SparseMatrix<uint> reservationTable;
+
+		public readonly SpaceTimeReservationInfo Info;
+		public readonly Actor Self;
+
+		public SpaceTimeReservation(Actor self, SpaceTimeReservationInfo info)
+		{
+			Info = info;
+			Self = self;
+
+			if (!Self.Owner.NonCombatant)
+				reservationTable = new SparseMatrix<uint>();
+			else
+				reservationTable = null;
+		}
+
+		public void Reserve(int x, int y, int t)
+		{
+			var wrappedT = t % Info.TimeLength;
+			reservationTable[x, y, wrappedT] = Self.ActorID;
+		}
+
+		public void Free(int x, int y, int t)
+		{
+			var wrappedT = t % Info.TimeLength;
+			reservationTable.RemoveKey(x, y, wrappedT);
+		}
+
+		public bool Check(int x, int y, int t)
+		{
+			var wrappedT = t % Info.TimeLength;
+			return reservationTable.ContainsKey(x, y, wrappedT);
+		}
+	}
+}
